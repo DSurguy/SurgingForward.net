271cc28a6eebdfb1f669d7ecae9cad0f65b7ea3f
Derek Surguy <Surguy.Derek@gmail.com>
2020-03-01 09:13:13 -0600

    Add intro paragraph and photo

--- next/src/views/Home.vue
+++ next/src/views/Home.vue
@@ -12,7 +12,28 @@
       <div class="hero__gradient"></div>
     </div>
     <div class="body">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="intro">
+        <div class="intro__text">
+          <p>
+            Hi! I'm Derek Surguy. I'm a front-end software engineer with 10 years of experience in web development. I use modern technologies like NodeJS and Vue to create innovative, intuitive and performant applications.
+          </p>
+          <p>
+            I have a passion for helping others realize their goals, and strive to enable the users of my software to be more efficient and discover new ways to use their technology.
+          </p>
+        </div>
+        <div class="intro__image">
+          <img src="@/assets/face.jpg" />
+        </div>
+      </div>
+      <div class="separator"></div>
+      <div class="connect">
+        <div class="connect__text">
+          Connect with me on any of these services
+        </div>
+      </div>
+      <!-- <StyledSocialLink icon={mailIconSrc} href="Surguy.Derek@gmail.com" linkPrefix={'mailto:'} />
+      <StyledSocialLink icon={githubIconSrc} href="https://github.com/DSurguy" />
+      <StyledSocialLink icon={linkedInIconSrc} href="https://linkedin.com/DSurguy" /> -->
     </div>
   </div>
 </template>
@@ -22,6 +43,7 @@
   font-family: 'Mukta', 'sans-serif';
   background-color: #0C1A1D;
   color: #fff;
+  padding-bottom: 200px;
 }
 .hero {
   position: relative;
@@ -92,17 +114,97 @@
   margin: 0 1rem;
 }
 .body {
+  z-index: 2;
   padding: 2rem;
+  position: relative;
+  margin: -110px auto 0 auto;
+  max-width: 960px;
+  background-color: #fff;
+  box-sizing: border-box;
+}
+.intro {
+  display: flex;
+  padding: 0 3rem;
+}
+.intro__text {
+  color: #000;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 1.8rem;
+  padding: 3rem;
+}
+.intro__image {
+  display: flex;
+  align-items: center;
+  padding: 3rem;
+}
+.intro__image img {
+  border-radius: 50%;
+}
+.separator {
+  height: 8px;
+  background-color: #efefef;
+  margin: 0 9rem;
+}
+.connect {
+  padding: 1.5rem;
+}
+.connect__text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  font-weight: 300;
+  color: #777;
+}
+
+@media (max-width: 960px) {
+  .body,
+  .intro__text{
+    padding: 1.5rem;
+  }
+  .intro__image {
+    align-items: flex-start;
+    padding: 1.5rem 0 1.5rem 3rem;
+  }
+}
+@media (max-width: 800px) {
+  .intro__text {
+    padding: 1.5rem 0;
+  }
+  .separator {
+    margin: 0 6rem;
+  }
+}
+
+@media (max-width: 640px) {
+  .intro {
+    flex-direction: column-reverse;
+  }
+  .intro__text {
+    padding: 1.5rem 0;
+  }
+  .intro__image {
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 1.5rem 0;
+  }
+  .separator {
+    margin: 0 3rem;
+  }
+  .connect__text {
+    padding: 0 3rem;
+    text-align: center;
+  }
 }
 </style>
 
-<script>
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+@Component
+export default class HomeView extends Vue {
+  
 }
 </script>

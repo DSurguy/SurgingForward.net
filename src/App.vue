1bc2a3936cefd07c758a6edec7d97c7687e34733
Derek Surguy <Surguy.Derek@gmail.com>
2021-06-14 10:23:56 -0500

    Basic homepage image

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Home />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 })
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #0C1A1D;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
 }
 </style>

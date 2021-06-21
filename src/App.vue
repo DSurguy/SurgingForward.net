4e0ecfb1c756c2f1c0c54ba21411fe79cc266221
Derek Surguy <Surguy.Derek@gmail.com>
2021-06-21 08:47:33 -0500

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <Home />
+  <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Home from './components/Home.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    Home
-  }
+  components: {}
 })
 </script>
 
